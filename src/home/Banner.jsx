2e056a72a2179cd1932575348450ea1e1fe08b31
weiwei0304 <106504956+weiwei0304@users.noreply.github.com>
2023-12-19 22:20:36 +0800

    1:10:49

---
+++ src/home/Banner.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import productData from '../products.json';
+const title = (
+  <h2>
+    Search Your One From <span>Thousand</span> of Products
+  </h2>
+);
+
+const desc = 'We have the largest collection of products';
+const bannerList = [
+  {
+    iconName: 'icofont-users-alt-4',
+    text: '1.5 Million Customers',
+  },
+  {
+    iconName: 'icofont-notification',
+    text: 'More then 2000 Marchent',
+  },
+  {
+    iconName: 'icofont-globe',
+    text: 'Buy Anything Online',
+  },
+];
+const Banner = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredProducts, setfilteredProducts] = useState(productData);
+
+  //   search functionality
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    setSearchInput(searchTerm);
+  };
+
+  return (
+    <div className="banner-section style-4">
+      <div className="container">
+        <div className="banner-content">
+          {title}
+          <form>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search your product..."
+              value={searchInput}
+              onChange={handleSearch}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Banner from './Banner';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
