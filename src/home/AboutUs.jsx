6390346119b060f6852ccd634b9df88bdc919c84
weiwei0304 <106504956+weiwei0304@users.noreply.github.com>
2024-01-22 10:48:26 +0800

    2:21:49
    2:21:49

--- src/home/AboutUs.jsx
+++ src/home/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CountUp from 'react-countup';
+import { Link } from 'react-router-dom';
 
 const subTitle = 'Why Choose Us';
 const title = 'Become a Marchant';
@@ -30,7 +31,7 @@ const AboutUS = () => {
     <div className="instructor-section style-2 padding-tb section-bg-ash">
       <div className="container">
         <div className="section-wrapper">
-          <div className="row">
+          <div className="row g-4 justify-content-center align-items-center row-cols-1 row-cols-md-2 row-cols-xl-3">
             <div className="col">
               {countList.map((val, i) => (
                 <div key={i} className="count-item">
@@ -56,6 +57,17 @@ const AboutUS = () => {
             <div className="col">
               <div className="instructor-content">
                 <p className="subtitle">{subTitle}</p>
+                <h2 className="title">{title}</h2>
+                <p>{desc}</p>
+                <Link to="/sign-up" className="lab-btn">
+                  {btnText}
+                </Link>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="instructor-thumb">
+                <img src="/src/assets/images/instructor/01.png" alt="" />
               </div>
             </div>
           </div>
