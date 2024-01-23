aaee3b8990c787ea16e732c41992d2db4221551e
weiwei0304 <weiwendy0430@gmail.com>
2024-01-23 18:23:10 +0800

    2:26:34

--- src/home/AppSection.jsx
+++ src/home/AppSection.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const btnText = 'Sign up for Free!!';
+const title = 'Shop Anytime, Anywhere';
+const desc =
+  'Take shop on your any device with our app & learn all time what want. Just download & install & start to shopping';
 
 const AppSection = () => {
-  return <div>AppSection</div>;
+  return (
+    <div className="app-section padding-tb">
+      <div className="container">
+        <div className="section-header text-center">
+          <Link to="sign-up" className="lab-btn mb-4">
+            {btnText}
+          </Link>
+          <h2 className="title">{title}</h2>
+          <p>{desc}</p>
+        </div>
+
+        <div className="section-wrapper">
+          <ul className="lab-ul">
+            <li>
+              <a href="#">
+                <img src="/src/assets/images/app/01.jpg" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="/src/assets/images/app/02.jpg" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppSection;
---
+++ src/home/Sponsor.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const sponsorList = [
+  {
+    imgUrl: '/src/assets/images/sponsor/01.png',
+  },
+  {
+    imgUrl: '/src/assets/images/sponsor/02.png',
+  },
+  {
+    imgUrl: '/src/assets/images/sponsor/03.png',
+  },
+  {
+    imgUrl: '/src/assets/images/sponsor/04.png',
+  },
+  {
+    imgUrl: '/src/assets/images/sponsor/05.png',
+  },
+  {
+    imgUrl: '/src/assets/images/sponsor/06.png',
+  },
+];
+
+const Sponsor = () => {
+  return <div>Sponsor</div>;
+};
+
+export default Sponsor;
