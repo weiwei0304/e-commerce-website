f6fb37589d17b6456b71f6a9aed0c535f0fabf69
weiwei0304 <106504956+weiwei0304@users.noreply.github.com>
2024-02-09 13:13:28 +0800

    3:43:31

--- src/shop/Search.jsx
+++ src/shop/Search.jsx
@@ -2,7 +2,26 @@ import React, { useState } from 'react';
 
 const Search = ({ products, gridList }) => {
   const [searchTerm, setSearchTerm] = useState('');
-  return <div>Search</div>;
+  const filterProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  );
+  return (
+    <div className="widget widget-search">
+      <form className="search-wrapper mb-3">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          defaultValue={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">
+          <i className="icofont-search-2"></i>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
